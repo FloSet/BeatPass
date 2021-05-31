ecb0a863cc621c2cee6c4c046a97e077c6f5620f
FloSet <florian-112@gmx.de>
2021-05-31 18:24:37 +0200

    Changed Name of Levels
    new Names are required to work, NextLevel depending on LevelNames (Word with 5 characters and a number)

--- Assets/Script/NextLevel.cs
+++ Assets/Script/NextLevel.cs
@@ -1,23 +1,38 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class NextLevel : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public Text noFurtherLevel;
+
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        Debug.Log("Nächstes Level");
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex +1);
+        string currentSceneName = SceneManager.GetActiveScene().name;
+        int currentSceneNumber;
+
+        try
+        {
+            currentSceneNumber = Int32.Parse(currentSceneName.Substring(5));
+        }
+		catch (Exception e)
+        {
+            currentSceneNumber = 1;
+            Debug.Log("Fehler in der Levelbennung!\n" + e);
+        }
+
+        if(SceneManager.GetSceneByName("Level" + (currentSceneNumber + 1)).IsValid())
+        {
+            Debug.Log(currentSceneNumber);
+            SceneManager.LoadScene("Level" + (currentSceneNumber + 1));
+            Debug.Log("Nächstes Level: " + (currentSceneNumber + 1));
+        }
+        else{
+            noFurtherLevel.enabled = true;
+        }
     }
 }
