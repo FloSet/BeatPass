6e2c1c4a136130028cdfdf6a72c4129b02840d67
FloSet <florian-112@gmx.de>
2021-05-31 23:17:12 +0200

    Reverted to old working version

--- Assets/Script/NextLevel.cs
+++ Assets/Script/NextLevel.cs
@@ -7,32 +7,9 @@ using UnityEngine.UI;
 
 public class NextLevel : MonoBehaviour {
 
-    public Text noFurtherLevel;
-
-
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        string currentSceneName = SceneManager.GetActiveScene().name;
-        int currentSceneNumber;
-
-        try
-        {
-            currentSceneNumber = Int32.Parse(currentSceneName.Substring(5));
-        }
-		catch (Exception e)
-        {
-            currentSceneNumber = 1;
-            Debug.Log("Fehler in der Levelbennung!\n" + e);
-        }
-
-        if(SceneManager.GetSceneByName("Level" + (currentSceneNumber + 1)).IsValid())
-        {
-            Debug.Log(currentSceneNumber);
-            SceneManager.LoadScene("Level" + (currentSceneNumber + 1));
-            Debug.Log("Nächstes Level: " + (currentSceneNumber + 1));
-        }
-        else{
-            noFurtherLevel.enabled = true;
-        }
+        Debug.Log("Nächstes Level");
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 }
