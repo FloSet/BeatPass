3c5775ad7d900df359671a26d9d2c525ea38d014
FloSet <florian-112@gmx.de>
2021-06-02 17:19:04 +0200

    Added comments

--- Assets/Script/NextLevel.cs
+++ Assets/Script/NextLevel.cs
@@ -9,36 +9,38 @@ public class NextLevel : MonoBehaviour {
 
     public Text noFurtherLevel;
 
-
+    //Wird ausgelöst wenn jemand den Zieltrigger berührt
     private void OnTriggerEnter2D(Collider2D collision)
     {
         List<string> scenesInBuild = new List<string>();
+
+        //Durchloopt alle im Build enthaltenen Ziele
         for (int i = 0; i < SceneManager.sceneCountInBuildSettings; i++)
         {
+            //Ermittlung des Pfades der Scene
             string scenePath = SceneUtility.GetScenePathByBuildIndex(i);
+            //Pfad abschneiden und nur Dateiname übrig lassen
             int lastSlash = scenePath.LastIndexOf("/");
+            //Entfernung von Dateiendungen und hinzufügen zur Liste
             scenesInBuild.Add(scenePath.Substring(lastSlash + 1, scenePath.LastIndexOf(".") - lastSlash - 1));
         }
 
-
-        foreach (String s in scenesInBuild)
-        {
-            Debug.Log(s);
-        }
-
-        string currentSceneName = SceneManager.GetActiveScene().name;
         int currentSceneNumber;
 
         try
         {
-            currentSceneNumber = Int32.Parse(currentSceneName.Substring(5));
+            //Ermittlung der Levelnummer
+            currentSceneNumber = Int32.Parse(SceneManager.GetActiveScene().name.ToString().Substring(5));
         }
         catch (Exception e)
         {
+            //Im Fall eines Fehlers bei Level 1 anfangen
+            //TODO: Bessere Lösung, z. B. Fehlermeldung dafür evtl. noFurtherLevel Feld in allgemeines Statusfeld umwandeln
             currentSceneNumber = 1;
             Debug.Log("Fehler in der Levelbennung!\n" + e);
         }
 
+        //Level mit nächst höhere Nummer laden oder "Keine weiteren Levels" ausgeben
         if (scenesInBuild.Contains("Level" + (currentSceneNumber + 1)))
         {
             SceneManager.LoadScene("Level" + (currentSceneNumber + 1));
