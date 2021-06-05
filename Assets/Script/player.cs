7e5e2c0bd158a8438f9c0aa8c58ec46ac7e9ae10
FloSet <florian-112@gmx.de>
2021-06-06 00:56:20 +0200

    Added basic mechanic

--- Assets/Script/player.cs
+++ Assets/Script/player.cs
@@ -10,10 +10,23 @@ public class Player : MonoBehaviour {
     int lives = 3;
     public Text scoreText;
     public Text livesText;
+    public GameObject bullet;
+    public float laserSpeed;
+    public Transform spawnPoint;
 
     private void Start()
     {
         Score = 0;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            Debug.Log("Fire");
+            GameObject nBullet = Instantiate(bullet, spawnPoint.position, spawnPoint.rotation);
+            nBullet.GetComponent<Rigidbody2D>().AddForce(Vector3.right * laserSpeed);
+        }
     }
 
     public void Death()
